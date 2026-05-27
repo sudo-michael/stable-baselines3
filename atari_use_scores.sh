69714feee7c8698327a9dfc4a4b7ce0bc8e15f1f
Michael Lu <lu.michael@outlook.com>
2026-05-27 02:06:45 -0700

    new script

--- atari_use_scores.sh
+++ atari_use_scores.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #SBATCH -J Atari
-#SBATCH --time=0-06:00
+#SBATCH --time=0-12:00
 #SBATCH --mem=4G
 #SBATCH --cpus-per-task=4
-#SBATCH --array=0-4%5
-#SBATCH --gpus=a100_1g.5gb:1
+#SBATCH --array=0-14%15
 #SBATCH --output=cc/%x_%a.out
 
 module restore sb3
@@ -13,16 +12,21 @@ source ~/sb3/bin/activate
 # ---------------------
 # Parameters for this task
 # ---------------------
+PARAMS=("30 0"  "30 1" "30 2" "30 3" "30 4" "40 0"  "40 1" "40 2" "40 3" "40 4" "50 0"  "50 1" "50 2" "50 3" "50 4" )
+
 TASK_ID=${SLURM_ARRAY_TASK_ID}
+PARAM="${PARAMS[$TASK_ID]}"
+read boundary_y seed <<< "$PARAM"
 
 # ---------------------
 # Run experiment
 # ---------------------
 python train_atari.py \
     --use_wandb False \
-    --exp_name atari_scores \
+    --exp_name atari_scores_changing_y_boundary \
     --slurm_task_id $TASK_ID \
-    --seed $TASK_ID \
-    --use_objects False
+    --seed $seed \
+    --use_objects False \
+    --pong_boundary_y $boundary_y \
 
 
